817eb9fe71e863996fa09f1634db2001d3d341dd
tennisgirladitis <147354869+tennisgirladitis@users.noreply.github.com>
2024-05-21 18:39:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+prediction_1 = "";
+prediction_2 = "";
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function takeSnapshot(data_uri) {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
